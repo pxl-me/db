c1bc2cd39b466073f578a8bdb72543bf5413fd0d
Ihor <tsake.love@gmail.com>
2019-12-03 16:29:56 +0200

    env.bd

--- db.js
+++ db.js
@@ -1,3 +1,7 @@
 module.exports = {
+<<<<<<< Updated upstream
     'url' : 'mongodb+srv://user:user@cluster0-5adfx.mongodb.net/test?retryWrites=true&w=majority'
+=======
+    'MONGOLAB_URI' : 'mongodb+srv://user:user@cluster0-5adfx.mongodb.net/test?retryWrites=true&w=majority'
+>>>>>>> Stashed changes
 }
